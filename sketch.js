1bcdf4c52cb39813d2872ec35d3f01db3e30a04e
Hirakvaghela <82459596+Hirakvaghela@users.noreply.github.com>
2021-04-22 11:48:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var Ghost,GhostImage
+var Tower,TowerImage
+var PLAY = 1;
+var END = 0;
+var gameState=PLAY
+var Door,DoorImage
+var left,right,leftImage,rightImage
+var score=0
+var DoorGroup
+
+
+function preload(){
+  GhostImage=loadImage("ghost.png")
+  TowerImage=loadImage("tower.png")
+  DoorImage=loadImage("door.png")
+  leftImage=loadImage("leftarrow.png")
+  rightImage=loadImage("rightarrow.png")
+
+}
+
+function setup() {
+  createCanvas(600,600)
+  Tower = createSprite(300,300);
+  Tower.addImage(TowerImage);
+  Ghost = createSprite(300,500);
+  Ghost.addImage(GhostImage);
+  left = createSprite(38,390);
+  left.addImage(leftImage);
+  right = createSprite(530,392);
+  right.addImage(rightImage);
+  Ghost.scale = 0.3
+  Tower.scale = 0.9
+  right.scale = 0.4
+  left.scale = 0.4
+  Tower.velocityY = 3;
+  Ghost.dbug = true;
+  DoorGroup = new Group();
+  
+  score=0
+}
+
+function draw() {
+  background("white")
+   text("Score: "+ score, 500,50);
+  
+  if(gameState==PLAY){
+    score = score+ Math.round(getFrameRate()/60);
+  
+  if (keyDown("left_arrow")){
+    Ghost.x = Ghost.x -5;
+  }
+  if (keyDown("right_arrow")){
+    Ghost.x = Ghost.x +5;
+  }
+  doors();
+  if (Tower.y>600){
+    Tower.y = 400;
+  }
+    if (mousePressedOver(right)) {
+    ( Ghost.x = Ghost.x +5);
+    }
+    
+    if (mousePressedOver(left)) {
+    ( Ghost.x = Ghost.x -5);
+    }
+  if(DoorGroup.isTouching(Ghost)||Ghost.y>600){
+    gameState = END;
+  }
+ 
+  drawSprites();
+}
+   if(gameState==END){
+    background("red");
+     textSize(40);
+     fill("blue");
+     stroke("Black");
+     text("GAME OVER",200,200)
+  }
+  
+}
+
+function doors(){
+  if (frameCount % 100 ==0 ){
+    Door = createSprite(Math.round(random(100,500)),-50)
+    Door.addImage(DoorImage);
+    Door.dbug = true;
+    Door.velocityY = 3;
+    Door.scale = 0.5
+    Door.lifetime = 250;
+    Door.depth = Ghost.depth
+    Ghost.depth +=1 ;
+    DoorGroup.add(Door);
+  }
+
+
+ 
+}
